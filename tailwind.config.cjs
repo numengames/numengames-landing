820ae0bd14a647e59cdc1b9f26e53402a8fa4d07
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-08-13 20:53:13 +0200

    feat: Remove functionalities and embed an iframe with the oncyber website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["geist", 'sans-serif'],
-        mono: ["geist", 'serif'],
-      },
-      textDecorationColor: ['responsive', 'hover', 'focus'],
-      textDecorationThickness: ['responsive', 'hover', 'focus'],
-      textUnderlineOffset: ['responsive', 'hover', 'focus'], 
-    },
+    extend: {},
     variants: {
-      extend: {
-        textDecoration: ['hover', 'focus'],
-      },
+      extend: {},
     },
   },
-  plugins: [require("@tailwindcss/typography")],
 };
